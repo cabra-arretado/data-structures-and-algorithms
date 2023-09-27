9c55b55273ce62491ba868f6cf975416febbd310
Felipe <80273544+cabra-arretado@users.noreply.github.com>
2023-09-27 18:06:07 -0500

    singly linked list in ts

--- python/singly_linked_list.py
+++ python/singly_linked_list.py
@@ -15,10 +15,10 @@ class SinglyLinkedList:
 		if self.head is None:
 			self.head = new_node
 			return
-		l_node = self.head
-		while (l_node.next):
-			l_node.next = l_node
-		l_node.next = new_node
+		last_node = self.head
+		while (last_node.next):
+			last_node.next = last_node
+		last_node.next = new_node
 		self.length += 1
 	
 	def prepend(self, val):
@@ -29,6 +29,6 @@ class SinglyLinkedList:
 
 	def remove_at(self, val, index):
 		pass
-	
+    
 	def get(index):
 		pass
---
+++ typescript/SinglyLinkedList.ts
@@ -0,0 +1,132 @@
+class Node<T> {
+  public value: T;
+  public next?: Node<T> | undefined;
+
+  constructor(value: T, next?: Node<T>) {
+    this.value = value;
+    this.next = next ? next : undefined;
+  }
+}
+
+export default class SinglyLinkedList<T> {
+  public length: number;
+  public head?: Node<T> | undefined;
+
+  constructor() {
+    this.length = 0
+  }
+
+  prepend(item: T): void {
+    let node = new Node(item)
+    this.length++;
+    if (this.head) {
+      node.next = this.head
+      this.head = node
+      return
+    }
+    this.head = node
+  }
+
+  insertAt(item: T, idx: number): void {
+    let new_node = new Node(item);
+    this.length++;
+    let prev_node = this.get_node(idx);
+    if (!prev_node) {
+      return
+    }
+    new_node.next = prev_node.next;
+    prev_node.next = new_node;
+  }
+
+  append(item: T): void {
+    let node = new Node(item)
+    this.length++;
+    let curr = this.head
+    if (curr) {
+      while (curr.next) {
+        curr = curr.next
+      }
+      curr.next = node
+      return
+    }
+    this.head = node
+  }
+
+  remove(item: T): T | undefined {
+    // To fix that
+    let idx = this.findFirstItemIndex(item)
+    if (!idx) {
+      return
+    }
+    return this.removeAt(idx)
+  }
+
+  get(idx: number): T | undefined {
+    return this.get_node(idx)?.value
+  }
+
+  get_node(idx: number): Node<T> | undefined {
+    let curr = this.head
+    if (idx > this.length - 1 || curr == undefined || idx < 0) {
+      return undefined
+    }
+    for (let i = 0; i <= idx; i++) {
+      if (i == idx) {
+        return curr
+      }
+      if (!curr.next) {
+        return undefined
+      }
+      curr = curr.next
+    }
+    return undefined
+  }
+
+  removeAt(idx: number): T | undefined {
+    if (idx > this.length - 1 ||  idx < 0 || this.length == 0) {
+      return 
+    }
+    if (idx == 0) {
+      let val = this.head?.value
+      this.head = this.head?.next
+      this.length--;
+      return val;
+    }
+    let prev_node = this.get_node(idx - 1)
+
+    if (!prev_node) {
+      return
+    }
+    let target_node = prev_node.next
+    if (!target_node) {
+      return
+    }
+    prev_node.next = target_node.next
+    this.length--;
+    return target_node.value
+  }
+
+  // Get the index of the first occurrence of the item in the list
+  findFirstItemIndex(item: T): number | undefined {
+    let curr = this.head
+    if (!curr) {
+      return undefined;
+    }
+    let i = 0;
+    while (true){
+      if (curr.value === item) {
+        return i;
+      }
+      else {
+        if (curr.next) {
+          curr = curr.next;
+          i++;
+          continue;
+        }
+        break;
+      }
+    }
+    return undefined;
+  }
+}
+
