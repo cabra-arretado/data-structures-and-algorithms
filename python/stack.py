f84ac756abb1c0157c2af1a05787d06c0254c52d
Felipe <80273544+cabra-arretado@users.noreply.github.com>
2023-10-06 17:58:02 -0500

    stack in python

---
+++ python/stack.py
@@ -0,0 +1,34 @@
+class Node:
+    value = None
+    previous = None
+
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Stack:
+    tail = None
+    length = 0
+
+    def push(self, item):
+        new_node = Node(item)
+        self.length += 1
+        if self.tail is None:
+            self.tail = new_node
+            return
+        new_node.previous = self.tail
+        self.tail = new_node
+
+    def pop(self):
+        if self.tail is None:
+            return None
+        self.length -= 1
+        value = self.tail.value
+        self.tail = self.tail.previous
+        return value
+
+
+
+    def peek(self):
+        if self.tail:
+            return self.tail.value
