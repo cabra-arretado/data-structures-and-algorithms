deabd14f31a6c3f1e4be178ce3c9b5a599ddcfa8
Felipe <80273544+cabra-arretado@users.noreply.github.com>
2023-10-09 16:21:39 -0500

    starting arrayList python

---
+++ python/array_list.py
@@ -0,0 +1,29 @@
+# from array import array
+
+class ArrayList:
+    array = []
+    size = 0
+    capacity = 0
+    def __init__(self, capacity):
+        self.array = []
+        self.size = 0
+        self.capacity = capacity
+
+    def append():
+        pass
+
+    def prepend():
+        pass
+
+    def insert_at():
+        pass
+
+    def remove_at():
+        pass
+
+    def remove():
+        pass
+
+    def get():
+        pass
+
