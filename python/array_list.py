4c226ada41cecf15951cd58dfb01e7d86e553014
Felipe <80273544+cabra-arretado@users.noreply.github.com>
2023-10-10 22:11:34 -0500

    python arraylist append

--- python/array_list.py
+++ python/array_list.py
@@ -1,16 +1,29 @@
 # from array import array
 
+
 class ArrayList:
-    array = []
+    capacity = 5
     size = 0
-    capacity = 0
-    def __init__(self, capacity):
+    # for the sake of simplicity, we will use python's list, but in reality, we should use an array.array instance
+    array = [None] * capacity
+    def __init__(self, capacity = 5):
         self.array = []
         self.size = 0
         self.capacity = capacity
 
-    def append():
-        pass
+    def append(self, val):
+        if self.size == self.capacity:
+            self.capacity *= 2
+            new_array = [None] * self.capacity
+            new_array[0:self.size] = self.array
+            self.array = new_array
+            self.array[self.size] = val
+            self.size += 1
+            return
+        self.array[self.size] = val
+        self.size += 1
+
+
 
     def prepend():
         pass
