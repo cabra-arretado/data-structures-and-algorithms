f9a2070452dbfb55ccdc6316ceb9f15f5400dadc
Felipe <80273544+cabra-arretado@users.noreply.github.com>
2023-10-18 22:01:33 -0500

    fast implement of remove

--- python/array_list.py
+++ python/array_list.py
@@ -10,34 +10,34 @@ class ArrayList:
         self.capacity = capacity
         # For the sake of using arra.array, we'll use -1 as a sentinel value
         self.array = array('i', [-1] * self.capacity)
-        self.size = 0
+        self.length = 0
 
     def __len__(self):
-        return self.size
+        return self.length
 
     def __getitem__(self, index):
         self.get(index)
 
     def append(self, val):
-        if self.size == self.capacity:
+        if self.length == self.capacity:
             self.capacity *= 2
             new_array = [None] * self.capacity
-            new_array[:self.size] = self.array
+            new_array[:self.length] = self.array
             self.array = new_array
-            self.array[self.size] = val
-            self.size += 1
+            self.array[self.length] = val
+            self.length += 1
             return
-        self.array[self.size] = val
-        self.size += 1
+        self.array[self.length] = val
+        self.length += 1
 
     def prepend(self, val):
-        if self.size == self.capacity:
+        if self.length == self.capacity:
             self.capacity *= 2
             new_array = [None] * self.capacity
-            new_array[:self.size] = self.array[1:self.size + 1]
+            new_array[:self.length] = self.array[1:self.length + 1]
         else:
-            array[1:self.size + 1] = array[:self.size]
-        self.size += 1
+            array[1:self.length + 1] = array[:self.length]
+        self.length += 1
         new_array[0] = val
 
     def insert_at(self, index, val):
@@ -47,9 +47,19 @@ class ArrayList:
         pass
 
     def remove(self, val):
-        pass 
+        if not self.array:
+            return False
+        for i in range(self.length):
+            if self.array[i] == val:
+                if len(self) == 1:
+                    self.length = 0
+                    self.array[0] = -1
+                    return True
+                self.array[i:] = self.array[i+1:]
+                return True
+
     def get(self, index):
 
-        if index >= self.size or index < 0:
+        if index >= self.length or index < 0:
             raise IndexError
         return self.array[index]
